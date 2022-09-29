f7e32c2a5e915b8e013b7f4f2198b7f0a3081ffa
shivakshi <shivakshi0660.be20@chitkara.edu.in>
2022-09-29 19:09:13 +0530

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Who invented C++?",
+    answer: "Bjarne Stroustrup",
+    options: [
+      "Bjarne Stroustrupr",
+      "Ken Thompson",
+      "Brian Kernighan",
+      "Dennis Ritchie"
+    ]
+  },
+    {
+    numb: 2,
+    question: " What is C++?",
+    answer: " C++ supports both procedural and object oriented programming language",
+    options: [
+      "C++ is an object oriented programming language",
+      "C++ supports both procedural and object oriented programming language",
+      "C++ is a procedural programming language",
+      " C++ is a functional programming language"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Which of the following is the correct syntax of including a user defined header files in C++?",
+    answer: "#include “userdefined”",
+    options: [
+      "#include [userdefined]",
+      "#include “userdefined”",
+      "#include <userdefined.h>",
+      "#include <userdefined>"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which of the following user-defined header file extension used in c++?",
+    answer: "h",
+    options: [
+      "hg",
+      "cpp",
+      "h",
+      "hf"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Which of the following approach is used by C++?",
+    answer: "Bottom-up",
+    options: [
+      " Left-right",
+      "Right-left",
+      "Bottom-up",
+      "Top-down"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
